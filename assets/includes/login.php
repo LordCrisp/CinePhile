1b73859d626156a19879af7bb8a12841a3d3c56e
kris783c <kristianholmsten@gmail.com>
2017-11-13 12:09:18 +0100

    Login sys
    :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b: :b:

--- assets/includes/login.php
+++ assets/includes/login.php
@@ -1,14 +1,5 @@
-<?php require 'dbInfo.php' ?>
 <?php require 'header.php'  ?>
-
-<form method="post">
-  <input type="text" name="email" placeholder="E-Mail" required>
-  <input type="password" name="password" placeholder="Password" required>
-  <button class="btn btn-lg btn-primary" type="submit">Login</button>
-  <a class="btn btn-lg btn-primary" href="register.php">Register</a>
-</form>
-
-<<?php
+<?php
 session_start();
   require('dbInfo.php');
 
@@ -16,20 +7,34 @@ if(isset($_POST['email']) and isset($_POST['password'])) {
 $email = $_POST['email'];
 $password = $_POST['password'];
 
-$query = "SELECT * FROM `user` WHERE email='$email' and password='$password'";
 
-$result = mysqli_query($dbConnect, $query) or die(mysqli_error($dbConnect))
+$query = "SELECT * FROM `users` WHERE `email` = '$email' and `password` = '$password' ";
 
+$result = mysqli_query($dbConnect, $query) or die(mysqli_error($dbConnect));
+$count = mysqli_num_rows($result);
 
-
+if($count == 1){
+  $_SESSION['email'] = $email;
+}
 }
 
 
+if (isset($_SESSION['email'])){
+  $email = $_SESSION['email'];
+  echo "Welcome  $email";
+  echo "<a class='btn btnlg btn-primary' href='logoff.php'>Log off</a>";
+
+}
+
+?>
+<form method="post">
+  <input type="text" name="email" placeholder="E-Mail" required>
+  <input type="password" name="password" placeholder="Password" required>
+  <button class="btn btn-lg btn-primary" type="submit">Login</button>
+  <a class="btn btn-lg btn-primary" href="register.php">Register</a>
+</form>
 
 
-
-
- ?>
 
 
 
