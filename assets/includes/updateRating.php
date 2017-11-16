19d451e5d9c476b8fdc33f4d4ee71a37c4af0973
LordCrisp <lord_crisp@hotmail.com>
2017-11-16 15:19:09 +0100

    user rating virker... ska ha' variabler til at blive brugt :unicorn:

--- assets/includes/updateRating.php
+++ assets/includes/updateRating.php
@@ -1,12 +1,35 @@
 <?php
 session_start();
-$updateRating = $_POST['$ratingSelect'];
 require 'dbInfo.php';
 
-$updateQuery = "UPDATE user_rating SET rating = ".$updateRating." WHERE user_id = ".$_SESSION['user_id'].", movie_id = ".$movieSelect." ";
-$result = $dbConnect->query($updateQuery);
+$updateRating = $_POST['ratingSelect'];
 
-$updateQuery = "UPDATE user_rating SET rating = ".$updateRating." WHERE user_id = ".$_SESSION['user_id'].", movie_id = ".$movieSelect." ";
-$result = $dbConnect->query($updateQuery);
+/* -- CHECK IF ALREADY RATED -- */
+$checkQuery = "SELECT * FROM user_rating WHERE user_id = 4 && movie_id = 3";
+$checkResult = $dbConnect->query($checkQuery);
+$checkRow = $checkResult -> fetch_assoc();
 
-print_r($updateQuery);
+
+if(isset($checkRow)) {
+    /* -- UPDATE RATING -- */
+
+    /*$updateQuery = "UPDATE user_rating SET rating = ".$updateRating." WHERE user_id = ".$_SESSION['user_id']." && movie_id = ".$movieSelect." ";
+    $result = $dbConnect->query($updateQuery);*/
+
+    $updateQuery = "UPDATE user_rating SET rating = ".$updateRating." WHERE user_id = 4 && movie_id = 3";
+    $updateResult = $dbConnect->query($updateQuery);
+}
+
+/* -- CREATE RATING -- */
+else {
+    /*$createQuery = "INSERT INTO user_rating (user_id, movie_id, rating) VALUES (".$_SESSION['user_id'].", ".$movieSelect.", ".$updateRating.") ";
+    $createResult = $dbConnect->query($createQuery);*/
+
+    $createQuery = "INSERT INTO user_rating (user_id, movie_id, rating) VALUES (4, 3, ".$updateRating.") ";
+    $createResult = $dbConnect->query($createQuery);
+}
+
+
+
+
+
--- userRatingSelect.php
+++ userRatingSelect.php
@@ -1,8 +1,8 @@
 <?php session_start(); ?>
-<form method="post" action="assets/updateRating.php">
-    <button name="ratingSelect" value="1"><i class="material-icons">star_border</i></button>
-    <button name="ratingSelect" value="2"><i class="material-icons">star_border</i></button>
-    <button name="ratingSelect" value="3"><i class="material-icons">star_border</i></button>
-    <button name="ratingSelect" value="4"><i class="material-icons">star_border</i></button>
-    <button name="ratingSelect" value="5"><i class="material-icons">star_border</i></button>
+<form method="post" action="assets/includes/updateRating.php">
+    <button name="ratingSelect" value="1"><i class="material-icons">star_border</i>1</button>
+    <button name="ratingSelect" value="2"><i class="material-icons">star_border</i>2</button>
+    <button name="ratingSelect" value="3"><i class="material-icons">star_border</i>3</button>
+    <button name="ratingSelect" value="4"><i class="material-icons">star_border</i>4</button>
+    <button name="ratingSelect" value="5"><i class="material-icons">star_border</i>5</button>
 </form>
